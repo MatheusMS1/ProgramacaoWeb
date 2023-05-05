affa1920021189c3a8755d7d86f261af6fae29e8
MatheusMS1 <120744198+MatheusMS1@users.noreply.github.com>
2023-05-05 19:26:48 -0300

    Delete Atividade09 directory

--- Atividade09/programacaoWeb/Atividade03/js/script.js
+++
@@ -1,6 +0,0 @@
-const a = Number(prompt("Digite o salario"))
-const b = Number(prompt("Digite o porcentual de alteração do salário"))
-
-salarioFinal = a + a*(b/100)
-
-document.write(`o salário final é: ${salarioFinal}`)
--- Atividade09/programacaoWeb/README.md
+++
@@ -1,2 +0,0 @@
-# programacaoWeb
-Repositório para atividades da disciplina de Programação Web - ADS4
